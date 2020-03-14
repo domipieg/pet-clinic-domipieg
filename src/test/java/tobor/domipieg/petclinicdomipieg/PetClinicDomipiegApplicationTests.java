d0fc06d2678549a1ae32f9024a4a76880be801fc
Dominik <domi.pieg@gmail.com>
2020-03-14 21:52:58 +0400

    init commit

---
+++ src/test/java/tobor/domipieg/petclinicdomipieg/PetClinicDomipiegApplicationTests.java
@@ -0,0 +1,13 @@
+package tobor.domipieg.petclinicdomipieg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicDomipiegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
